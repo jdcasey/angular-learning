9663fb60a446f517e09140101d44052ee7e34e67
John Casey <jdcasey@commonjava.org>
2014-05-28 13:27:51 -0500

    Adding snippets to demo rendering a deptree with search

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,23 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+  $scope.nodes = [
+    {'item': 'org.apache.maven:maven:3.1.1',
+     'subs': [
+      {'item': 'org.apache.maven:maven-core:3.1.1'},
+      {'item': 'com.google.guice:guice:2.0.1'},
+     ],
+    },
+    {'item': 'org.apache.maven:maven-compat:3.1.1',
+     'subs': [
+        {'item': 'org.apache.maven:maven-artifact:3.1.1'},
+        {'item': 'org.apache.maven:maven-plugin-api:3.1.1'},
+        {'item': 'org.apache.maven:maven-compat:3.1.1'},
+        {'item': 'org.sonatype.sisu:sisu:1.0',
+         'subs':[
+          {'item': 'com.google.guice:guice:2.1.1'},
+        ]},
+     ]
+    }
   ];
 });
